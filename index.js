545be47d67d2d4169805783bd3c5c9260f9c576d
MALEK <MALEK.KANAAN@OUTLOOK.COM>
2017-02-17 14:50:17 +0100

    secound commit always better

--- index.js
+++ index.js
@@ -3,40 +3,25 @@ const express = require('express');
 const app = express();
 app.use(require('body-parser').json());
 
-var todo = [{
-    "id": 0,
-    "task": "cooking"
-  },
-  {
-    "id": 2,
-    "task": "cooking"
-  },
-  {
-    "id": 3,
-    "task": "cooking"
-  },
-  {
-    "id": 4,
-    "task": "cooking"
-  },
-  {
-    "id": 5,
-    "task": "cooking"
-  },
-  {
-    "id": 6,
-    "task": "cooking"
-  },
-  {
-    "id": 7,
-    "task": "cooking"
-  }];
+var todo = [];
 var uniqueId = 0;
-
 
 
 app.get('/todo',(req, res, next) => {
 	res.status(200).json(todo);
+	console.log("all tasks sent");
+});
+
+app.get('/todo/:id',(req, res, next) => {
+	for (var i = 0; i < todo.length; i++) {
+		if (todo[i].id == req.params.id) {
+			res.status(200).json(todo[i]);
+			console.log("task " + todo[i].id + " sent");
+			return;
+		}
+	}
+	console.log("error " + req.params.id + " not found");
+	res.status(404).json({"message":"Not Found"});
 });
 
 app.post('/todo',(req, res, next) => {
@@ -46,37 +31,46 @@ app.post('/todo',(req, res, next) => {
 	newTask.task = req.query.task;
 	todo.push(newTask);
 	res.status(201).json(todo);	
-	console.log("task "+ req.query.task + "added");
-
+	console.log("task " + req.query.task + " added");
 });
 
 app.patch('/todo/:id',(req, res, next) => {
-	console.log(req.params.id);
 	if (req.params.id !== undefined){
-		for (var i =0; i < todo.length; i++) {
+		for (var i = 0; i < todo.length; i++) {
 			if (todo[i].id == req.params.id) {
-			todo[i].task = req.query.task;
-			res.status(200).json(todo[i]);
-			console.log("task number "+todo[i].id+" got updated");
+				todo[i].task = req.query.task;
+				res.status(200).json(todo[i]);
+				console.log("task " + todo[i].id + " got updated");
+				return;
 			}
 		}
-		console.log("error not found");
+		console.log("error " + req.params.id + " not found");
 		res.status(404).json({"message":"Not Found"});
 	}
 });
 
 app.delete('/todo/:id',(req, res, next) => {
-	for (var i =0; i < todo.length; i++) {
-		console.log(todo[i].id);
-		if (todo[i].id == req.params.id) {
-			todo.splice(i,1);
+	
+	if(req.params.id !== "all"){
+		for (var i = 0; i < todo.length; i++) {
+			if (todo[i].id == req.params.id) {
+				todo.splice(i,1);
+				res.status(200).json(todo);
+				console.log("task " + req.params.id + " got deleted");
+				return;
+			}
 		}
+		console.log("error " + req.params.id + " not found");
+		res.status(404).json({"message":"Not Found"});
+	} else {
+		todo = [];
+		console.log("all tasks got deleted");
+		res.status(200).json({"message":"all tasks got deleted"});
 	}
-	res.status(200).json(todo);
-	console.log("task number "+req.params.id+" got deleted");
+
 });
 
-//------------------------------------------------
+
 function uniqueIdMaker(){
 	return uniqueId++;
 }
